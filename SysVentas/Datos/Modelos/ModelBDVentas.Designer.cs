4d36d1968a1c4034d1fb741227f2adb6b525028c
Jean Bustamante <jeanx32@gmail.com>
2020-07-22 23:33:38 -0500

    Subida Marca, Linea

--- SysVentas/Datos/Modelos/ModelBDVentas.Designer.cs
+++ SysVentas/Datos/Modelos/ModelBDVentas.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jrojamal\Desktop\SISTEMA VENTAS - COMSEG\FUENTE\SysVentas\Datos\Modelos\ModelBDVentas.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\tienda\Desktop\Pry\Comseg\SysVentas\Datos\Modelos\ModelBDVentas.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
